dc39ada5e89ac09f20990b8410d5363370d66500
matias <matias.vanonen@gmail.com>
2018-11-22 00:07:02 +0000

    edit bash scrip

--- bash/run.sh
+++ bash/run.sh
@@ -7,3 +7,7 @@ sudo apt-get -y install git salt-master salt-minion
 echo "Configuring Git"
 git config --global user.email "matias.vanonen@gmail.com"
 git config --global user.name "matias"
+
+echo "Configuring the slave"
+echo -e "master: 192.168.1.70\nid: matias"|sudo tee /etc/salt/minion
+sudo systemctl restart salt-minion.service
